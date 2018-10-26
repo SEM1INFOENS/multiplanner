6ac68946a4a7dac5133afc759ecd5e34312d4ff8
Delphine Salvy <delphine.salvy@icloud.com>
2018-10-26 16:03:25 +0200

    Correcting errors of models.py 10/10

--- groups/models.py
+++ groups/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from accounting.models import Transaction
 
 
@@ -11,27 +12,34 @@ class Group(models.Model):
     '''
     name = models.CharField(max_length=200, blank=True)
     members = models.ManyToManyField(User)
+
     def get_transaction_list(self):
+        '''Return the list of transactions of the group'''
         return self.transactionforgroup_set.all()
+
     def __repr__(self):
         '''Enables to display an Event in a convenient way'''
         return "name : {}".format(self.name)
 
-    
+
 class TransactionForGroup(Transaction):
     '''A transaction that was made for a certain group
     '''
-    def validate_TransacGroup(group):
+    def validate_transac_group(self, group):
+        '''Checks that all the beneficiaries of the transaction are in the group'''
         try:
             gp_members = group.members.all()
             ben = self.beneficiaries.all()
-            for b in ben:
-                assert (b in gp_members)
+            for beneficiary in ben:
+                assert beneficiary in gp_members
             return group
         except:
-            raise ValidationError("some beneficiaries of a TransactionForGroup are not in the group")
+            message = "Some beneficiaries of a TransactionForGroup are not in the group."
+            raise ValidationError(message)
+
     group = models.ForeignKey(Group, on_delete=models.PROTECT,
-                              validators=[validate_TransacGroup])
+                              validators=[validate_transac_group])
     def __repr__(self):
         '''Enables to display a TransactionForGroup in a convenient way'''
-        return "{}, group : <{}>".format(super(TransactionForGroup, self).__repr__(), self.group.__repr__())
+        return "{}, group : <{}>".format(super(TransactionForGroup, self). \
+            __repr__(), self.group.__repr__())
