6a7ec7b26e99668733aaa03325a952355ee7b2d5
Delphine Salvy <delphine.salvy@icloud.com>
2018-11-09 18:21:48 +0100

    relationship_matrix (not tested yet)

--- agenda/models.py
+++ agenda/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 #from django.core.exceptions import ValidationError
 from groups.models import Group
 from accounting.models import Transaction
-
+from relationships.models import SecretMark
 
 class TimeRange(models.Model):
     '''A time range is defined by its beginning and its duration.'''
@@ -17,7 +17,7 @@ class TimeRange(models.Model):
         '''Creates a Time Range starting at dateTR with duration durationTR'''
         tr = cls(date=dateTR, duration=durationTR)
         return tr
-        
+
     def __repr__(self):
         #return "Begins at {}, lasts {}".format(self.date, self.duration)
         return "Between {} and {} ( lasts {} )".format(self.date, (self.date+self.duration), \
@@ -52,16 +52,32 @@ class Event(models.Model):
         event.transactions.add(*transactions)
         return event
 
-    def get_transaction_list(self):
-        '''Give the list of the transactions in the event.'''
-        return self.transactionforevent_get.all()
-
     def __repr__(self):
         '''Enables to display an event in a convenient way.'''
 
         return "date : {}, place : {}, creator : {}, administrators : {}, attendees : {}, \
         invited : {}, transactions : {}".format(self.date, self.place, self.creator, \
             self.administrators, self.attendees, self.invited, self.transactions)
+
+
+    def relationship_matrix(self):
+        '''Returns M matrix of size n*n with n the number of attendees
+        with M[i][j] the secrete mark i gave j, 0 if no such mark exists'''
+        list_att = self.attendees.members.all()
+        n = len(list_att)
+
+        M = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            # I cannot find a way to do it without two loops, because a mark is between user1 
+            # and user2 and I don't find a way to know the number of user2 without being as 
+            # long as with a loop
+            for j in range(n):
+                try:
+                    M[i][j] = SecretMark.objects.get(user=list_att[i], marked_user=list_att[j])
+                except SecretMark.DoesNotExist:
+                    ()
+        return M
 
 
 # class TransactionForEvent(Transaction):
