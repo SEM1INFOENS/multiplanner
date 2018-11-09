c6c80664c662a50db3de4e7a1916a329c6907cd1
Delphine Salvy <delphine.salvy@icloud.com>
2018-11-09 17:28:30 +0100

    Real fix

--- relationships/tests.py
+++ relationships/tests.py
@@ -7,30 +7,30 @@ from relationships.models import *
 class GroupTestCase(TestCase):
     print("\n\n - running tests for the relationship app:\n")
 
-    # gt_links = {}
-    # users_list = []
+    gt_links = {}
+    users_list = []
     
-    # def setUp(self):    
-    #     a = User.objects.create_user(username='alice')
-    #     b = User.objects.create_user(username='bob')
-    #     c = User(username='carlotta')
-    #     c.save()
-    #     self.users_list = [a,b,c,]
+    def setUp(self):    
+        a = User.objects.create_user(username='alice')
+        b = User.objects.create_user(username='bob')
+        c = User(username='carlotta')
+        c.save()
+        self.users_list = [a,b,c,]
 
-    #     m1=SecretMark(a, b, 10)
-    #     m2=SecretMark(b, c, 9)
-    #     m1.save()
-    #     m2.save()
+        m1=SecretMark.create_new(a, b, 10)
+        m2=SecretMark.create_new(b, c, 9)
+        m1.save()
+        m2.save()
 
     def test_mark(self):
         u_qs=User.objects.all()
-        # for u in u_qs:
-        #     print("\n-user %s\nmarks given:" %u.username)
-        #     for m in u.secretmark_set.all():
-        #         print(" to %s: %i" %(m.marked_user.username, m.mark))
-        #         assert (m.user==u)
-
-        #     print("marks received:")
-        #     for m in u.marked_user_set.all():
-        #         print(" from %s: %i" %(m.user.username, m.mark))
-        #         assert (m.marked_user==u)
+        for u in u_qs:
+            print("\n-user %s\nmarks given:" %u.username)
+            for m in u.secretmark_set.all():
+                print(" to %s: %i" %(m.marked_user.username, m.mark))
+                assert (m.user==u)
+    
+            print("marks received:")
+            for m in u.marked_user_set.all():
+                print(" from %s: %i" %(m.user.username, m.mark))
+                assert (m.marked_user==u)
