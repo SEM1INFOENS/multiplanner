9aa08bbe5ce532b7bd0685c5280809b988cd21b4
Roman Kniazev <kameronton@gmail.com>
2018-11-09 13:52:26 +0100

    added basic presentation for users

--- presentation/views.py
+++ presentation/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from accounting.models import Transaction
+
+def index(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'users/index.html', context)
+
+def page(request, username):
+    context = {'transactions': Transaction.objects.filter(payer=User.objects.get(username=username)),
+               'username': username}
+    return render(request, 'users/page.html', context)
