bdd3032422b5a1c3ea6b1a1aaf160426c43375a8
PierreGtch <pierre.guetschel@gmail.com>
2018-11-23 17:09:43 +0100

    user's functions with django-friendship

--- presentation/functions.py
+++ presentation/functions.py
@@ -1,5 +1,6 @@
 from groups.models import Group
 from random import shuffle
+from friendship.models import Friend
 
 def queryset_to_list(Q):
     L = []
@@ -7,17 +8,16 @@ def queryset_to_list(Q):
         L.append(q)
     return L
 
-# def n_random_friends(user, n):
-#         '''Returns a random list of six friends of the User user'''
-#         friendqueryset = Friendships.objects.get(user=user).friend_list.all()
-
-#         friendlist = queryset_to_list(friendqueryset)
-
-#         if len(friendlist) <= n:
-#             return friendlist
-
-#         shuffle(friendlist)
-#         return friendlist[:n]
+def n_random_friends(user, n):
+    '''Returns a random list of six friends of the User user'''
+    
+    friendlist = Friend.objects.friends(user)
+    
+    if len(friendlist) <= n:
+        return friendlist
+    
+    shuffle(friendlist)
+    return friendlist[:n]
 
 def last_news(user):
-        user_groups = user.group_set.all()
+    user_groups = user.group_set.all()
--- presentation/tests.py
+++ presentation/tests.py
@@ -1,13 +1,24 @@
 from django.test import TestCase
 from presentation.models import *
 from .functions import *
+from friendship.models import Friend, FriendshipRequest
 # Create your tests here.
 
+def set_inclus(l1,l2):
+    for x in l1:
+        if x not in l2:
+            print(x)
+            return False
+    return True
+        
 class FunctionsTestCase(TestCase):
     print("\n\n - running tests for the presentation app:\n")
 
 
     def test_six_random_friends(self):
+
+        n = 6
+        
         print("six_random_friends")
 
         a = User.objects.create_user(username='a')
@@ -20,12 +31,20 @@ class FunctionsTestCase(TestCase):
         h = User.objects.create_user(username='h')
 
         L = [b, c, d, e, f, g, h]
-        friendship = Friendships.create_new(a, [], [])
-
-        for i in range(6):
-            friendship.friend_list.add(L[i])
-
-            assert(six_random_friends(a) == L[:i+1])
-
-        friendship.friend_list.add(h)
-        assert(len(six_random_friends(a)) == 6)
+        i = 0
+        for u2 in L:
+            Friend.objects.add_friend(
+                a,                    # The sender
+                u2,                   # The recipient
+                message='Hi! I (user a)  would like to add you (user u2)') 
+            f_req = FriendshipRequest.objects.get(to_user=u2)
+            f_req.accept()
+            print(Friend.objects.friends(a))
+            
+            i += 1
+            L2 = n_random_friends(a,n)
+            if i <= n :
+                assert( set_inclus(L2 , L[:i]) and set_inclus(L[:i], L2) )
+            else:
+                assert( set_inclus(L2 , L) and len(L2)==n )
+            
