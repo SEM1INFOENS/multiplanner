f85556322d4020f487ca1347d8537b8e5af0cd18
Delphine Salvy <delphine.salvy@icloud.com>
2018-11-23 16:20:06 +0100

    An old Frienships of ours remained... 4

--- presentation/functions.py
+++ presentation/functions.py
@@ -7,17 +7,17 @@ def queryset_to_list(Q):
         L.append(q)
     return L
 
-def n_random_friends(user, n):
-        '''Returns a random list of six friends of the User user'''
-        friendqueryset = Friendships.objects.get(user=user).friend_list.all()
+# def n_random_friends(user, n):
+#         '''Returns a random list of six friends of the User user'''
+#         friendqueryset = Friendships.objects.get(user=user).friend_list.all()
 
-        friendlist = queryset_to_list(friendqueryset)
+#         friendlist = queryset_to_list(friendqueryset)
 
-        if len(friendlist) <= n:
-            return friendlist
+#         if len(friendlist) <= n:
+#             return friendlist
 
-        shuffle(friendlist)
-        return friendlist[:n]
+#         shuffle(friendlist)
+#         return friendlist[:n]
 
 def last_news(user):
         user_groups = user.group_set.all()
