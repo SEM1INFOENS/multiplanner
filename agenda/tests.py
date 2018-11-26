f8ce0964f12452bb16cc56c1aff0eca80f95e4d9
Malo Perez <malo.perez@epfl.ch>
2018-11-26 15:39:55 +0100

    Corrected sit.py and added test

--- agenda/tests.py
+++ agenda/tests.py
@@ -20,17 +20,31 @@ class AgendaTestCase(TestCase):
         a = User.objects.create_user(username='alice')
         b = User.objects.create_user(username='bob')
         c = User(username='carllotta')
-        a.save()
-        b.save()
+        d = User(username = 'david')
+        e = User(username='emily')
+        f = User(username='fred')
+        e.save()
+        f.save()
         c.save()
-        self.users_list = [a,b,c,]
+        d.save()
+        self.users_list = [a,b,c,d,e,f,]
 
-        g = Group()
-        g.save()
-        g.members.add(*[a,b,c])
+        g1 = Group()
+        g1.save()
+        g1.members.add(*[a,b,c,d,])
+
+        g2 = Group()
+        g2.save()
+        g2.members.add(*[a,b,c,d,e,f,])
 
         m1 = SecretMark.create_new(a, b, -10)
         m1.save()
+
+        m2 = SecretMark.create_new(b,c,-10)
+        m2.save()
+
+        m3 = SecretMark.create_new(e, c, 10)
+        m3.save()
 
         e1 = Event(
             date=timezone.now(),
@@ -39,12 +53,26 @@ class AgendaTestCase(TestCase):
             time_end=timezone.now()+datetime.timedelta(hours=1),
             #place="ici",
             creator=a,
-            attendees=g,
+            attendees=g1,
         )
         e1.save()
-        e1.invited.add(*[a,b,c])
+        e1.invited.add(*[a,b,c,d])
         e1.administrators.add(a)
-        self.event_list=[e1]
+
+        e2 = Event(
+            date=timezone.now(),
+            time=timezone.now(),
+            date_end=timezone.now() + datetime.timedelta(days=1),
+            time_end=timezone.now() + datetime.timedelta(hours=1),
+            # place="ici",
+            creator=a,
+            attendees=g2,
+        )
+        e2.save()
+        e2.invited.add(*[a, b, c, d,e ,f ,])
+        e2.administrators.add(a)
+
+        self.event_list=[e1,e2]
 
     def test_event(self):
         for e in Event.objects.all():
@@ -52,10 +80,25 @@ class AgendaTestCase(TestCase):
             assert (e in self.event_list)
 
     def test_sitting(self):
-
         tables = [2,2]
 
+        #Case with 4 people
         assi = sitting(self.event_list[0],tables)
 
         assert(assi[0] == assi[2])
         assert(assi[1] != assi[0])
+        assert(assi[1] == assi[3])
+
+        #Case with 6 people
+
+        tables = [3,3]
+
+        assi = sitting(self.event_list[1],tables)
+
+        assert(assi[0] == assi[2])
+        assert(assi[2] ==  assi[4])
+        assert(assi[1] != assi[0])
+        assert(assi[1] == assi[3])
+        assert(assi[3] == assi[5])
+
+
