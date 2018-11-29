8fb2a72c6c3152104e3604d7fe6cf8cf3bf98ebb
PierreGtch <pierre.guetschel@gmail.com>
2018-11-29 10:55:32 +0100

    web #39

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:  python3 manage.py migrate --run-syncdb ; python3 manage.py runserver
+web: gunicorn multiplanner.wsgi
--- multiplanner/settings.py
+++ multiplanner/settings.py
@@ -153,5 +153,9 @@ LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 
 #To use heroku
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+import django_heroku
+django_heroku.settings(locals())
+
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
