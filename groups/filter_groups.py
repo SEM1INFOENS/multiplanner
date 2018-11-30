8c35c606d436a484ff40d079e154a6845ba7c3bd
kessaye <simon_leb_5@hotmail.com>
2018-11-30 16:10:28 +0100

    Working on group filtering

---
+++ groups/filter_groups.py
@@ -0,0 +1,18 @@
+''' Filtering functions for groups '''
+from django.db import models
+from django.contrib.auth.models import User
+from .models import *
+from agenda.models import *
+
+def all_groups_of_user (user):
+	return Group.objects.filter(members = user).order_by('name')
+
+
+def groups_not_in_events ():
+	att = Event.objects.all()
+	for i in att:
+		i.attendees
+	return Group.objects.difference(att)
+
+def groups_of_user(user) :
+	return groups_not_in_events().filter(members = user).order_by('name')
