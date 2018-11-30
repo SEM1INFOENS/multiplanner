d233c8749605ee87ad32bd5797a49f39148fea1a
PierreGtch <pierre.guetschel@gmail.com>
2018-11-30 15:57:44 +0100

    enhance code multiplanner #44

--- multiplanner/views.py
+++ multiplanner/views.py
@@ -1,3 +1,2 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-
+#from django.shortcuts import render, redirect
+#from django.contrib.auth.decorators import login_required
