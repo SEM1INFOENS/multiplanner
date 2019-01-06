1ddc09ebbd85ce5dbcb8364c78daf0181628df4b
PierreGtch <pierre.guetschel@gmail.com>
2019-01-06 01:04:56 +0100

    coquille

--- permissions/utils.py
+++ permissions/utils.py
@@ -1,6 +1,6 @@
 
 def get_default_permission_name(model, perm_name):
-    ''' returns the default permission name (edit, change, add or delete)
+    ''' returns the default permission name (view, change, add or delete)
     for a particular model
     these permissions are automaticaly created with the migrations '''
     s = model._meta.label_lower.split('.')
