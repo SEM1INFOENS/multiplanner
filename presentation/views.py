e6e2f38926dd51b69d4bd85c021e3f13c2c4825b
PierreGtch <pierre.guetschel@gmail.com>
2019-01-07 16:43:44 +0100

    Fix a bug

--- presentation/views.py
+++ presentation/views.py
@@ -12,7 +12,6 @@ from groups.models import Group
 from .functions import *
 from relationships import functions as rel
 from relationships.models import SecretMark
-from permissions.views import manage_app_admins_context
 
 @login_required
 def index(request):
@@ -45,6 +44,7 @@ def page(request, username):
     }
     rel_context = rel.friendship_context(user, user_page)
     context.update(rel_context)
+    from permissions.views import manage_app_admins_context
     admin_context = manage_app_admins_context(user, user_page)
     context.update(admin_context)
     return render(request, 'users/page.html', context)
