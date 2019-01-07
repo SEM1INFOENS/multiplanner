95b372016ae4fec3edd53b789ca37312205ae979
Roman Kniazev <kameronton@gmail.com>
2019-01-08 00:37:35 +0100

    added creation of partial transactions (#55)

--- accounting/forms.py
+++ accounting/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
+from djmoney.utils import Money
 from .models import *
+from math import ceil
 
 
 
@@ -8,11 +10,9 @@ class TransactionForm(ModelForm):
 
     class Meta:
         model = Transaction
-        fields = [] #'motive', 'date', 'payer', 'amount', 'beneficiaries']
+        fields = ['motive', 'date', 'payer', 'amount'] #'motive', 'date', 'payer', 'amount', 'beneficiaries']
         widgets = {
-            'motive' : forms.Textarea,
             'date' : forms.SelectDateWidget(),
-            'amoint' : forms.NumberInput
         }
 
     def __init__(self, *args, **kwargs):
@@ -20,13 +20,27 @@ class TransactionForm(ModelForm):
         between_members = kwargs.pop('between_members')
         super(TransactionForm, self).__init__(*args, **kwargs)
 
-        # if between_members:
-        #     self.fields['payer'].queryset = self._group.members
-        #     self.fields['beneficiaries'].queryset = self._group.members
+        if between_members:
+            self.fields['payer'].queryset = self._group.members
+            #self.fields['beneficiaries'].queryset = self._group.members
 
     def save(self, commit=True):
         inst = super(TransactionForm, self).save()
         self._group.transactions.add(inst)
         self._group.save()
+        
+        base = Money(ceil(inst.amount/len(self._group.members.all())*100)/100, inst.amount.currency)
+
+        parts = [base for _ in self._group.members.all()]
+
+        if (sum(parts) != inst.amount):
+            parts[-1]+= inst.amount - sum(parts)
+
+        for (member, part) in zip(self._group.members.all(), parts):
+            tr = TransactionPart()
+            tr.transaction = inst
+            tr.beneficiary = member
+            tr.amount = part
+            tr.save()
 
         return inst
