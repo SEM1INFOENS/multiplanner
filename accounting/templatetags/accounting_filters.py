8c1acdc66c1607891aa939bc4189a4748177b0e2
PierreGtch <pierre.guetschel@gmail.com>
2019-01-08 14:30:01 +0100

    coquille

--- accounting/templatetags/accounting_filters.py
+++ accounting/templatetags/accounting_filters.py
@@ -7,10 +7,6 @@ register = template.Library()
 @register.filter
 def display_transaction(tr):
     name = tr.__str__()
-<<<<<<< HEAD
     url = tr.get_absolute_url()
-=======
-    url = reverse('transaction_details', args=(tr.id,))
->>>>>>> cf4b60ff255150906e506af9919692c6cfc0d5dd
     str = "<a href=\"{}\">{}</a>".format(url, name)
     return mark_safe(str)
