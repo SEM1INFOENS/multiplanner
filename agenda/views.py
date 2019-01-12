1c52ac7a9dc073fba8d99782f6d421c9c1397b2b
PierreGtch <pierre.guetschel@gmail.com>
2019-01-12 11:31:59 +0100

    move date_format_ics to agenda.functions #5 #45 #47

--- agenda/views.py
+++ agenda/views.py
@@ -16,28 +16,7 @@ from accounting import resolution
 from guardian.decorators import permission_required_or_403
 from permissions.utils import get_default_permission_name
 from permissions.forms import PermGroupForm
-
-
-def date_format_ics(date, time):
-    '''Converts date to date in ICS format'''
-    def add_zeros(s, year=False):
-        '''Adds the right number of zeros before the string s so that len(s) = 2 (4 if it is a year)'''
-        if year:
-            return (4 -len(s))*'0' + s
-
-        return (2-len(s))*'0' + s
-
-    if time == None:
-        time = datetime.time.min
-
-    s1 = str(date.year)
-    s2 = str(date.month)
-    s3 = str(date.day)
-    s4 = str(time.hour)
-    s5 = str(time.minute)
-    s6 = str(time.second)
-    return add_zeros(s1) + add_zeros(s2) + add_zeros(s3) + "T" + add_zeros(s4) + add_zeros(s5) + add_zeros(s6) + "Z"
-
+from .functions import date_format_ics
 
 @login_required
 def create_event(request):
