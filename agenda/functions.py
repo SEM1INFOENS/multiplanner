0daeeaf4c12b15f840dc1a85040a3b9f6a8bbf09
PierreGtch <pierre.guetschel@gmail.com>
2019-01-12 14:26:50 +0100

    formating functions #73

--- agenda/functions.py
+++ agenda/functions.py
@@ -19,3 +19,22 @@ def date_format_ics(date, time):
     s5 = str(time.minute)
     s6 = str(time.second)
     return add_zeros(s1) + add_zeros(s2) + add_zeros(s3) + "T" + add_zeros(s4) + add_zeros(s5) + add_zeros(s6) + "Z"
+
+def date_format_moment(date, time):
+    '''for FullCalandar'''
+    if time == None:
+        time = datetime.time.min
+    t = time.strftime('%H:%M:%S')
+    d = date.strftime('%Y-%m-%d')
+    return "{} {}".format(d,t)
+
+def color_format_css(r,g,b):
+    rr = '{:02x}'.format(r%256)
+    gg = '{:02x}'.format(g%256)
+    bb = '{:02x}'.format(b%256)
+    return '#{}{}{}'.format(rr,gg,bb)
+
+def color_complement(r,g,b):
+    ''' returns the complementary of an rgb color '''
+    f = lambda x : 256 - (x%256)
+    return (f(r), f(g), f(b))
