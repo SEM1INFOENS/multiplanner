f07f402570fa6296920cabac1b6fde782319db7b
Roman Kniazev <kameronton@gmail.com>
2019-01-16 16:25:55 +0100

    Add two tests
    One for group creation (also checks if editing doesn't create a new one)
    and one for transaction creation

--- multiplanner/tests.py
+++ multiplanner/tests.py
@@ -1,13 +1,20 @@
 
 # Integration tests
 
-from django.test import LiveServerTestCase
+from groups.models import Group
+from accounting.models import Transaction, TransactionPart
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.touch_actions import TouchActions
+from django.test.utils import override_settings
+from django.conf import settings
+import time
 
 
-class AccountTestCase(LiveServerTestCase):
+
+class AccountTestCase(StaticLiveServerTestCase):
 
     def setUp(self):
         self.selenium = webdriver.Firefox()
@@ -16,8 +23,8 @@ class AccountTestCase(LiveServerTestCase):
     def tearDown(self):
         self.selenium.quit()
         super(AccountTestCase, self).tearDown()
-
-    def test_register(self):
+    
+    def sub_test_register(self):
         selenium = self.selenium
         #Opening the link we want to test
         selenium.get(self.live_server_url+'/signup')
@@ -34,5 +41,46 @@ class AccountTestCase(LiveServerTestCase):
         password2.send_keys('cavacava')
         
         submit.click()
+        
         #check the returned result
-        assert 'Your current balance is' in selenium.page_source
+        assert 'Your current balance' in selenium.page_source
+
+
+
+    def sub_test_create_group(self):
+        selenium = self.selenium
+        element = selenium.find_element_by_link_text("Groups")
+        element.click()
+        element = selenium.find_element_by_link_text("create a new group")
+        element.click()
+
+        name = selenium.find_element_by_name('name')
+        name.send_keys('test_group')
+
+        selenium.find_element_by_xpath("//*[@id='id_currency']/option[3]").click()
+
+        selenium.find_element_by_xpath('//*[@id="id_members_field"]/option[2]').click()
+
+        selenium.find_element_by_xpath('//*[@id="content"]/form/input[3]').click()
+        selenium.find_element_by_xpath('//*[@id="content"]/a[1]').click()
+        selenium.find_element_by_xpath('//*[@id="content"]/form/input[3]').click()
+
+        assert len(Group.objects.all()) == 1
+        
+
+    def sub_test_create_transaction(self):
+        selenium = self.selenium
+
+        selenium.find_element_by_xpath('//*[@id="content"]/button').click()
+        selenium.find_element_by_xpath('//*[@id="id_payer"]/option[2]').click()
+        selenium.find_element_by_xpath('//*[@id="id_amount_0"]').send_keys('1.00')
+        selenium.find_element_by_xpath('//*[@id="exampleModal"]/div/div/div[2]/form/input[3]').click()
+        selenium.find_element_by_xpath('//*[@id="content"]/form/input[6]').click()
+
+        assert Transaction.objects.get(pk=1).amount.amount == 1.00
+
+    @override_settings(DEBUG=True)
+    def test_integration(self):
+        self.sub_test_register()
+        self.sub_test_create_group()
+        self.sub_test_create_transaction()
